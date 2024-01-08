6ebb2e4658313406b82426e2872f297b12b3452b
Kuba M <jtmuzyczuk@hotmail.com>
2024-01-09 00:23:55 +0100

    Tworzenie pierwszych warunków

--- ProgramData/AppOne/Program.cs
+++ ProgramData/AppOne/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ola";
+string sex = "female";
+int age = 29;
+
+if (sex == "female" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex != "female" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
+
+
